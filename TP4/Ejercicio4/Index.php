e25ba5fde513c397c8c059f79d88beac55ff4cc0
Joaco <jmurua03@gmail.com>
2024-05-22 16:06:10 -0300

    TP 4. Ejercicio 4

---
+++ TP4/Ejercicio4/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
+  <title>Document</title>
+</head>
+<body>
+
+  <form class="row g-1 mt-3 ms-2" action="check_name.php" method="post">
+    <div class="col-auto">
+      <input type="text" class="form-control" name="InputUser" placeholder="Usuario">
+    </div>
+    <div class="col-auto">
+        <input type="submit" class="btn btn-primary mb-3" value="Enviar">
+    </div>
+  </form>
+</body>
+</html>
